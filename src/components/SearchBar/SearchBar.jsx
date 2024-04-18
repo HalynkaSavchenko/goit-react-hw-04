64e0cb11da5af3a8f8d027c44581326ffefdda96
Halyna Savchenko <galasavchenko1@gmail.com>
2024-04-18 19:11:01 +0200

    start

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,10 @@
+export default function SearchBar() {
+    return(
+        <header>
+            <form action="">
+                <input type="text" autoComplete="off" autoFocus placeholder="Search images and photos" />
+                <button type="submit">Search</button>
+            </form>
+        </header>
+    )
+}
